bfae13514f294a8c6a670f25be12318f352e065d
johnson nyccolas <johnsonnyccolas21@gmail.com>
2017-09-27 13:38:53 -0300

    modulo 12

--- src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
+++ src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
@@ -1,10 +1,16 @@
 package com.algaworks.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
 
+import com.algaworks.pedidovenda.model.Categoria;
 import com.algaworks.pedidovenda.model.Produto;
 
 @Named
@@ -12,18 +18,38 @@ import com.algaworks.pedidovenda.model.Produto;
 public class CadastroProdutoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	@Inject
+	private EntityManager manager;
+	@Inject
 	private Produto produto;
-	
-	public CadastroProdutoBean() {
-		produto = new Produto();
+
+	private List<Categoria> categoriasRaizes = new ArrayList<>();
+
+	public void inicializar() {
+		System.out.println("Inicializando....");
+
+		// categoriasRaizes = manager.createQuery("from Categoria",
+		// Categoria.class).getResultList();
+
 	}
-	
+
 	public void salvar() {
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
 	}
 
 	public Produto getProduto() {
 		return produto;
+	}
+
+	public List<Categoria> getCategoriasRaizes() {
+		return categoriasRaizes;
+	}
+
+	public void setCategoriasRaizes(List<Categoria> categoriasRaizes) {
+		this.categoriasRaizes = categoriasRaizes;
 	}
 
 }
