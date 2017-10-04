896cf004cc0d4279e6e1a8f32771fe605c3bdb03
johnson nyccolas <johnsonnyccolas21@gmail.com>
2017-10-04 15:45:20 -0300

    modulo 12.7

--- src/main/java/com/algaworks/pedidovenda/repository/Produtos.java
+++ src/main/java/com/algaworks/pedidovenda/repository/Produtos.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.NoResultException;
 
 import com.algaworks.pedidovenda.model.Produto;
+import com.algaworks.pedidovenda.util.jpa.Transactional;
 
 public class Produtos implements Serializable {
 
@@ -16,25 +17,19 @@ public class Produtos implements Serializable {
 	@Inject
 	private EntityManager manager;
 
+	
 	public Produto guardar(Produto produto) {
-		EntityTransaction trx = manager.getTransaction();
-		trx.begin();
-		
-		produto = manager.merge(produto);
-		
-		trx.commit();
-		
-		return produto;
+		return manager.merge(produto);
+
 	}
 
 	public Produto porSku(String sku) {
 		try {
 			return manager.createQuery("from Produto where upper(sku) = :sku", Produto.class)
-				.setParameter("sku", sku.toUpperCase())
-				.getSingleResult();
+					.setParameter("sku", sku.toUpperCase()).getSingleResult();
 		} catch (NoResultException e) {
 			return null;
 		}
 	}
-	
+
 }
