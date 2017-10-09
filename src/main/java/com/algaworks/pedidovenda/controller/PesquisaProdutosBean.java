03e96753b2f4c14d4adfb77504a99fc36730bd7f
johnson nyccolas <johnsonnyccolas21@gmail.com>
2017-10-09 16:10:15 -0300

    modulo 12.10

--- src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
+++ src/main/java/com/algaworks/pedidovenda/controller/PesquisaProdutosBean.java
@@ -10,27 +10,37 @@ import javax.inject.Named;
 import com.algaworks.pedidovenda.model.Produto;
 import com.algaworks.pedidovenda.repository.Produtos;
 import com.algaworks.pedidovenda.repository.filter.ProdutoFilter;
+import com.algaworks.pedidovenda.util.jsf.FacesUtil;
 
 @Named
 @ViewScoped
 public class PesquisaProdutosBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private Produtos produtos;
-	
+
 	private ProdutoFilter filtro;
 	private List<Produto> produtosFiltrados;
-	
+
+	private Produto produtoSelecionado;
+
 	public PesquisaProdutosBean() {
 		filtro = new ProdutoFilter();
 	}
-	
+
+	public void excluir() {
+		produtos.remover(produtoSelecionado);
+		produtosFiltrados.remove(produtoSelecionado);
+
+		FacesUtil.addInfoMessage("Produto " + produtoSelecionado.getSku() + " excluído");
+	}
+
 	public void pesquisar() {
 		produtosFiltrados = produtos.filtrados(filtro);
 	}
-	
+
 	public List<Produto> getProdutosFiltrados() {
 		return produtosFiltrados;
 	}
@@ -38,5 +48,13 @@ public class PesquisaProdutosBean implements Serializable {
 	public ProdutoFilter getFiltro() {
 		return filtro;
 	}
-	
+
+	public Produto getProdutoSelecionado() {
+		return produtoSelecionado;
+	}
+
+	public void setProdutoSelecionado(Produto produtoSelecionado) {
+		this.produtoSelecionado = produtoSelecionado;
+	}
+
 }
--- src/main/java/com/algaworks/pedidovenda/repository/Produtos.java
+++ src/main/java/com/algaworks/pedidovenda/repository/Produtos.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.PersistenceException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.Criteria;
@@ -16,6 +17,8 @@ import org.hibernate.criterion.Restrictions;
 
 import com.algaworks.pedidovenda.model.Produto;
 import com.algaworks.pedidovenda.repository.filter.ProdutoFilter;
+import com.algaworks.pedidovenda.service.NegocioException;
+import com.algaworks.pedidovenda.util.jpa.Transactional;
 
 public class Produtos implements Serializable {
 
@@ -28,35 +31,48 @@ public class Produtos implements Serializable {
 		return manager.merge(produto);
 	}
 
+	@Transactional
+	public void remover(Produto produto) {
+		try {
+
+			produto = porId(produto.getId());
+			manager.remove(produto);
+			manager.flush();
+
+		} catch (PersistenceException e) {
+			throw new NegocioException("Produto não pode ser excluido");
+		}
+
+	}
+
 	public Produto porSku(String sku) {
 		try {
 			return manager.createQuery("from Produto where upper(sku) = :sku", Produto.class)
-				.setParameter("sku", sku.toUpperCase())
-				.getSingleResult();
+					.setParameter("sku", sku.toUpperCase()).getSingleResult();
 		} catch (NoResultException e) {
 			return null;
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public List<Produto> filtrados(ProdutoFilter filtro) {
 		Session session = manager.unwrap(Session.class);
 		Criteria criteria = session.createCriteria(Produto.class);
-		
+
 		if (StringUtils.isNotBlank(filtro.getSku())) {
 			criteria.add(Restrictions.eq("sku", filtro.getSku()));
 		}
-		
+
 		if (StringUtils.isNotBlank(filtro.getNome())) {
 			criteria.add(Restrictions.ilike("nome", filtro.getNome(), MatchMode.ANYWHERE));
 		}
-		
+
 		return criteria.addOrder(Order.asc("nome")).list();
 	}
 
 	public Produto porId(Long id) {
-		
+
 		return manager.find(Produto.class, id);
 	}
-	
+
 }
